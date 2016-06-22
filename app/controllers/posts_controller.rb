9a6b0777967c41f101b5f039d8d32dec986cc966
GavinChiang <cchsuan@umich.edu>
2016-06-22 14:18:41 +0800

    add destroy and edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,11 +16,32 @@ class PostsController < ApplicationController
     else
       render 'new'
     end
-    
+
   end
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def upadte
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to root_path
   end
 
   private
