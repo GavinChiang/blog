31ea65ee74d88c586bbf657dd36ae120af9f78dd
GavinChiang <cchsuan@umich.edu>
2016-06-25 10:20:26 +0800

    add category

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  resources :categories
+  
   resources :posts do
     resources :comments
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160622083744) do
+ActiveRecord::Schema.define(version: 20160624071420) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "posts_count"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.string   "name"
