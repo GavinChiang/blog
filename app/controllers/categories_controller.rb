33e543274513958387197c2e7eef984710140e9c
GavinChiang <cchsuan@umich.edu>
2016-07-09 15:09:11 -0700

    add category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,38 @@
 class CategoriesController < ApplicationController
+
+  before_action :authenticate_user!
+
+  def index
+    # @categories = Category.all.order("create_at DESC")
+    @categories = Category.all
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to posts_path
+    else
+      render 'new'
+    end
+
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:title)
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ActiveRecord::Base
 	validates :body, presence: true
 
 	has_many :comments, dependent: :destroy
+	belongs_to :category
 end
