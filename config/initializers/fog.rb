64d230dd0d803ffd6a1c872d41f2b0e3e202a9f2
GavinChiang <cchsuan@umich.edu>
2016-08-24 12:00:52 -0700

    divide pic storage to development and proudction

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,9 +1,13 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-  	provider: ENV['carrier_wave_provider'],
-    aws_access_key_id: ENV['carrier_wave_aws_access_key_id'],
-    aws_secret_access_key: ENV['carrier_wave_aws_secret_access_key'],
-    region:  ENV['carrier_wave_region']   
-  }
-  config.fog_directory  = ENV['carrier_wave__BUCKET_NAME']                          # required
+	if Rails.env.production?
+	  config.fog_credentials = {
+	  	provider: ENV['carrier_wave_provider'],
+	    aws_access_key_id: ENV['carrier_wave_aws_access_key_id'],
+	    aws_secret_access_key: ENV['carrier_wave_aws_secret_access_key'],
+	    region:  ENV['carrier_wave_region']   
+	  }
+	  config.fog_directory  = ENV['carrier_wave__BUCKET_NAME']
+	else
+	  config.storage = :file
+    end  
 end
